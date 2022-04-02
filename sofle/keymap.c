bcc3d1d33f58685ca5fa8e89a0114fcf3961cdb6
dbeley <6568955+dbeley@users.noreply.github.com>
2022-04-02 19:11:28 +0200

    sofle: update mirror layer

--- sofle/keymap.c
+++ sofle/keymap.c
@@ -142,9 +142,9 @@ enum custom_keycodes {
  	 */
 	[_MIRROR] = LAYOUT( \
 	KC_6, KC_7, KC_8, KC_9, KC_0, KC_QUOT,                                    KC_GRV,        KC_1, KC_2, KC_3, KC_4, KC_5, \
-	KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,                                    LGUI_T(KC_TAB),KC_Q, KC_W, KC_E, KC_R, KC_T, \
+	KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,                                    KC_TAB,        KC_Q, KC_W, KC_E, KC_R, KC_T, \
 	KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,                                  LCTL_T(KC_ESC),KC_A, KC_S, KC_D, KC_F, KC_G, \
-	KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_ESC), _______,   _______, LSFT_T(KC_ESC),KC_Z, KC_X, KC_C, KC_V, KC_B, \
+	KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_ESC), _______,   _______, KC_LSFT,       KC_Z, KC_X, KC_C, KC_V, KC_B, \
       	  KC_QWERTY, KC_LALT, KC_LCTRL, KC_SPC, KC_LOWER,                    KC_RAISE, KC_SPC, KC_RCTRL, KC_RALT, KC_QWERTY \
 	),
 
