c3ebe8b753d8b357f8f022d4750eabff0d3d9f51
dbeley <6568955+dbeley@users.noreply.github.com>
2023-03-20 11:37:43 +0100

    Update ymdk_np21

--- ymdk_np21/keymap.c
+++ ymdk_np21/keymap.c
@@ -42,11 +42,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY_GAMING_ALT] = LAYOUT_ortho_6x4(
         DF(_QWERTY_GAMING), DF(_GAMING), DF(_NUM), DF(_QWERTY),
-        _______,  _______,  _______,  KC_1,
-        _______,  _______,  _______,  KC_2,
-        _______,  _______,  _______,  KC_3,
-        _______,  _______,  _______,  KC_4,
-        _______,  _______,  KC_T,     KC_5
+        _______,  _______,  KC_TAB,   KC_1,
+        _______,  KC_J,     _______,  KC_2,
+        _______,  KC_K,     _______,  KC_3,
+        _______,  KC_L,     _______,  KC_4,
+        KC_N,     KC_I,     KC_T,     KC_5
         ),
 
 [_GAMING] = LAYOUT_ortho_6x4(
